f179746ba18b135e09e0162433e0be3fe54472ac
brunosana <brunosa@dcomp.ufs.br>
2022-01-23 15:57:41 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt';
 import { BcryptAdapter } from './bcrypt-adapter';
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await new Promise(resolve => resolve('hashed_value'));
+  },
+}));
+
 interface SutTypes {
   sut: BcryptAdapter;
 }
@@ -20,5 +26,12 @@ describe('Bcrypt Adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.encrypt('any_value');
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
+  });
+
+  test('Should return a hash on success', async () => {
+    const { sut } = makeSut();
+
+    const hash = await sut.encrypt('any_value');
+    expect(hash).toBe('hashed_value');
   });
 });
