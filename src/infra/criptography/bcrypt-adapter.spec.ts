d5004e3a69abb2336088b9448e0f025366b74f2e
brunosana <brunosa@dcomp.ufs.br>
2022-01-23 17:01:28 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -34,4 +34,14 @@ describe('Bcrypt Adapter', () => {
     const hash = await sut.encrypt('any_value');
     expect(hash).toBe('hashed_value');
   });
+
+  test('Should throws if bcrypt throws', async () => {
+    const { sut } = makeSut();
+
+    // eslint-disable-next-line
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => (new Promise((resolve, reject) => reject(new Error()))));
+
+    const promise = sut.encrypt('any_value');
+    await expect(promise).rejects.toThrow();
+  });
 });
