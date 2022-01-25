d4fa05a47358667a88878d503aa735f4182daeeb
brunosana <brunosa@dcomp.ufs.br>
2022-01-24 21:05:21 -0300

    feat: ensure requests return json as default content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,9 +1,11 @@
 import { Express } from 'express';
 import { bodyParser } from '../middlewares/body-parser';
+import { contentType } from '../middlewares/content-type';
 import { cors } from '../middlewares/cors';
 
 const midldewares = (app: Express): void => {
   app.use(cors);
+  app.use(contentType);
   app.use(bodyParser);
 };
 
