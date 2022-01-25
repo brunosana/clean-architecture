ea2bd2480c6e03f299d34009d36d787265230c63
brunosana <brunosa@dcomp.ufs.br>
2022-01-24 22:01:38 -0300

    refactor: connect to mongodb before integration tests

--- src/main/config/setupRoutes.ts
+++ src/main/config/setupRoutes.ts
@@ -3,10 +3,10 @@ import fg from 'fast-glob';
 
 const setupRoutes = (app: Express): void => {
   const router = Router();
+  app.use('/api', router);
   fg.sync('**/src/main/routes/**routes.ts').map(async file =>
     (await import(`../../../${file}`)).default(router),
   );
-  app.use('/api', router);
 };
 
 export { setupRoutes };
